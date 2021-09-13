4ff992329a3c5d4ba7ee7ebe12c031a3864fd657
Keyu Zhang <keyuz4@uci.edu>
2021-09-13 15:34:21 -0700

    can select what to respond

--- parse/ParseNotOnCampus.py
+++ parse/ParseNotOnCampus.py
@@ -7,17 +7,21 @@ TARGET_STRING_PREFIX = "mailto:" + SETTINGS["to"]
 
 class ParseNotOnCampus(HTMLParser):
     """parse the link for 'not on campus today' """
-    def __init__(self):
+    def __init__(self, response_type):
         super().__init__()
         self._target = None
         self._found_link = False
+        self._found_link_count = 0
+        self.response_type = response_type
 
     def handle_starttag(self, tag, attrs):
         if (not self._found_link
                 and tag == "a"
                 and attrs[0][1].startswith(TARGET_STRING_PREFIX)):
-            self._target = attrs[0][1]
-            self._found_link = True
+            if self._found_link_count == self.response_type:
+                self._target = attrs[0][1]
+                self._found_link = True
+            self._found_link_count += 1
 
     def get_result(self):
         if self._target is None:
@@ -25,8 +29,8 @@ class ParseNotOnCampus(HTMLParser):
         return self._target
 
 
-def parse_respond_mail(mail_html: str) -> dict:
-    parser = ParseNotOnCampus()
+def parse_respond_mail(mail_html: str, response_type: int) -> dict:
+    parser = ParseNotOnCampus(response_type)
     parser.feed(mail_html)
     response_query = parser.get_result()
     return parse_qs(response_query)
